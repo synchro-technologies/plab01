08ea6860f1e373c4fd12bf2e1ecceff80d3f0c02
SebastRodriguez <Sebasrodruy@gmail.com>
2019-12-20 10:58:43 -0500

    Playbooks and Scripts

--- Scripts/Install_All.sh
+++ Scripts/Install_All.sh
@@ -2,4 +2,7 @@
 
 # Run all playbooks
 
-ansible-playbook 
+ansible-playbook /home/osboxes/Synchro_Labs/Playbooks/Initial_Conf.yml --extra-vars "num=$1" -i /home/osboxes/Synchro_Labs/Playbooks/Inventory.txt
+ansible-playbook /home/osboxes/Synchro_Labs/Playbooks/Install_Git.yml --extra-vars "num=$1" -i /home/osboxes/Synchro_Labs/Playbooks/Inventory.txt
+ansible-playbook /home/osboxes/Synchro_Labs/Playbooks/Install_Ansible.yml --extra-vars "num=$1" -i /home/osboxes/Synchro_Labs/Playbooks/Inventory.txt
+ansible-playbook /home/osboxes/Synchro_Labs/Playbooks/Install_Docker.yml --extra-vars "num=$1" -i /home/osboxes/Synchro_Labs/Playbooks/Inventory.txt
